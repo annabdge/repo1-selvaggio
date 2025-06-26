39c79e033777a3562215074916e9b839e283423c
annabdge <anna.berti.14-1@accademiadigitaleliguria.it>
2025-06-26 17:15:56 +0200

    Calcolatrice e Test
    console app calcolatrice con test

---
+++ LibCalcolatrice/LibCalcolatrice/Calcolatrice.cs
@@ -0,0 +1,22 @@
+﻿namespace LibCalcolatrice;
+
+public class Calcolatrice
+{
+    public int somma(int a1, int a2)
+    {
+        return a1 + a2;
+    }
+
+    public int sottrai(int a1, int a2)
+    {
+        return a1 - a2;
+    }
+
+    public int moltiplica(int a1, int a2)
+    {
+        return a1 * a2;
+    }
+
+
+
+}
---
+++ LibCalcolatrice/TestProjectCalcolatrice/Test1.cs
@@ -0,0 +1,33 @@
+﻿namespace TestProjectCalcolatrice;
+using LibCalcolatrice;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestSomma()
+    {
+        var calc = new Calcolatrice();
+        var risultato = calc.somma(3, 5);
+        Assert.Equals(8, risultato);
+    }
+
+    [TestMethod]
+    public void TestSommaZero()
+    {
+        var calc = new Calcolatrice();
+        var risultato = calc.somma(3, 0);
+        Assert.AreEqual(3, risultato);
+    }
+
+    [TestMethod]
+    public void TestSommaRelativo()
+    {
+        var calc = new Calcolatrice();
+        var risultato = calc.somma(3, -1);
+        Assert.AreEqual(2, risultato);
+    }
+
+}
